089842c4253413c58e6da3a4ef74d110ec1571fe
bfactor <bfactor@andrew.cmu.edu>
2017-12-03 16:47:48 -0500

    Finish organization

--- convertEvalTestData.py
+++
@@ -1,8 +0,0 @@
-from scipy.io import loadmat
-import numpy as np
-
-matFile = loadmat('imgs_eval_test.mat')
-np.save('imgs_eval.npy',matFile['evalSet'])
-np.save('imgs_mask_eval.npy',matFile['evalLabelSet'])
-np.save('imgs_test.npy',matFile['testSet'])
-np.save('imgs_mask_test.npy',matFile['testLabelSet'])
