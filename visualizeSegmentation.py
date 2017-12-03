fd7b437f45a56a56093ba3a1b57a8f9111b4c85b
bfactor <bfactor@andrew.cmu.edu>
2017-12-03 12:54:43 -0500

    Modified result_plot to accept filename argument, modified visualizeSegmentation for set of 10 rather than 30 validation images

--- visualizeSegmentation.py
+++ visualizeSegmentation.py
@@ -9,16 +9,16 @@ filename = sys.argv[1]
 pickle_out = pickle.load(open(filename,'rb'))
 
 fig = plt.figure(1)
-grid = ImageGrid(fig, 111, nrows_ncols=(5,6),axes_pad = 0.1)
-for i in range(30):
+grid = ImageGrid(fig, 111, nrows_ncols=(2,5),axes_pad = 0.1)
+for i in range(10):
     grid[i].imshow(pickle_out[i,:,:,0],cmap='gray_r')
     grid[i].axis('off')
     grid[i].set_xticks([])
     grid[i].set_yticks([])
 
 fig2 = plt.figure(2)
-grid = ImageGrid(fig2, 111, nrows_ncols=(5,6),axes_pad = 0.1)
-for i in range(30):
+grid = ImageGrid(fig2, 111, nrows_ncols=(2,5),axes_pad = 0.1)
+for i in range(10):
     grid[i].imshow(pickle_out[i,:,:,1],cmap='gray_r')
     grid[i].axis('off')
     grid[i].set_xticks([])
