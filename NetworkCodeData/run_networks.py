3dab8767ab59468223138c6c64228907b5fe99a2
Brad Factor <brad.factor@gmail.com>
2017-12-05 05:46:37 -0500

    Added network running script and commited results from early/mid/fuse/fuse2 with 300 epochs

---
+++ NetworkCodeData/run_networks.py
@@ -0,0 +1,11 @@
+print('Running residual')
+import ContextualCNN_residual
+
+print('Running residual 2')
+import ContextualCNN_residual2
+
+print('Running earlyfuse')
+import ContextualCNN_earlyfuse
+
+print('Running middlefuse')
+import ContextualCNN_middlefuse
